16a4dce94401bc55d0b0b69f3155cf04441c7ea6
wisang <wseom7@gmail.com>
2023-07-05 23:04:05 +0900

    preprocessor and volatile

--- lesson5/main.c
+++ lesson5/main.c
@@ -1,24 +1,28 @@
+#include "lm4f120h5qr.h"
+
+/*
 #define RCGCGPIO (*((unsigned int*)0x400FE608U)) // clock gating control
 
 #define GPIOF_BASE 0x40025000U
 #define GPIOF_DIR (*((unsigned int*)(GPIOF_BASE + 0x400U)))
 #define GPIOF_DEN (*((unsigned int*)(GPIOF_BASE + 0x51CU)))
 #define GPIOF_DATA (*((unsigned int*)(GPIOF_BASE + 0x3FCU)))
+*/
 
 int main() {
-  RCGCGPIO = 0x20U;   // enable clock for GPIO_F
-  GPIOF_DIR = 0x0EU;  // set pins 1,2, and 3 as outputs
-  GPIOF_DEN = 0x0EU;
+  SYSCTL_RCGCGPIO_R = 0x20U;   // enable clock for GPIO_F
+  GPIO_PORTF_DIR_R = 0x0EU;  // set pins 1,2, and 3 as outputs
+  GPIO_PORTF_DEN_R= 0x0EU;
   
   while(1) {
-    GPIOF_DATA = 0x02U;
+    GPIO_PORTF_DATA_R = 0x02U;
   
-    int counter = 0;
+    int volatile counter = 0;
     while(counter < 1000000) {
       ++counter;
     }
   
-    GPIOF_DATA = 0x00U;
+    GPIO_PORTF_DATA_R = 0x00U;
     counter = 0;
     while(counter < 1000000) {
       ++counter;
