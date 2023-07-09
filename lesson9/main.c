6f29f133f153e4c5151325c471aa25b7c126b3a0
wisang <wseom7@gmail.com>
2023-07-09 11:40:08 +0900

    file separation

---
+++ lesson9/main.c
@@ -0,0 +1,27 @@
+#include "lm4f120h5qr.h"
+#include "delay.h"
+
+#define LED_RED (1U << 1)
+#define LED_BLUE (1U << 2)
+#define LED_GREEN (1U << 3)
+
+ int main() {
+  SYSCTL_RCGCGPIO_R |= (1U << 5); //0x20U;   // enable clock for GPIO_F
+  
+  SYSCTL_GPIOHBCTL_R |= (1U << 5); //enable AHB for GPIO
+  GPIO_PORTF_AHB_DIR_R |= (LED_BLUE | LED_RED | LED_GREEN); //0x0EU;  // set pins 1,2, and 3 as outputs
+  GPIO_PORTF_AHB_DEN_R |= (LED_BLUE | LED_RED | LED_GREEN); //0x0EU;
+  
+  GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE;
+  
+  while(1) {
+  
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED;
+    delay(1000000);
+  
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = 0;
+    delay(500000);
+  }
+  
+  return 0;
+}
